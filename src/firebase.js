fe5efd65307b501265bd9329a34078018ee6d4f5
mihir7121 <mihir.nikam1@gmail.com>
2024-07-31 18:24:07 -0700

    add environment file

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+import dotenv from 'dotenv';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDVtLhhjx-1obK7h1At594v_HawNo7l4Do",
-  authDomain: "inventory-management-app-f0bbd.firebaseapp.com",
-  projectId: "inventory-management-app-f0bbd",
-  storageBucket: "inventory-management-app-f0bbd.appspot.com",
-  messagingSenderId: "1053219634348",
-  appId: "1:1053219634348:web:fb1805ba0afc34a0a5f309",
-  measurementId: "G-2RS6CJPX5L"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
